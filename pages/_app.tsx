f8dd6f7fc26839184e00cab326510179971a5a76
tiavina-mika <tiavinamika@gmail.com>
2020-10-28 09:43:50 +0300

    react-jss installation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import type { AppProps /* , AppContext */ } from 'next/app';
-import '../styles/globals.css';
+import { ThemeProvider } from 'react-jss';
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+import '../styles/globals.css';
+import '../styles.css';
+
+const theme = {
+  colors: {
+    dark: '#000000',
+    white: '#ffffff',
+    primary: '#0c0c0c',
+    secondary: '#898880',
+    secondaryLight: '#C5C3BC',
+    accent: '#80722A',
+  },
+};
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
 
 export default MyApp;
